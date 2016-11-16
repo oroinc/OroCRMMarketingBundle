d02524632353064db8e724fbc2e6ed34dc501100
Balazs Csaba <csaba.balazs@evozon.com>
2016-11-16 10:47:32 +0200

    CRM-6464: Move Tracking dependencies - Moved platform dependencies to marketing package - WIP Moving CRM dependencies

--- src/Oro/Bundle/TrackingBundle/Migrations/Schema/OroTrackingBundleInstaller.php
+++ src/Oro/Bundle/TrackingBundle/Migrations/Schema/OroTrackingBundleInstaller.php
@@ -6,23 +6,37 @@ use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\MigrationBundle\Migration\Extension\RenameExtension;
+use Oro\Bundle\MigrationBundle\Migration\Extension\RenameExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-use Oro\Bundle\TrackingBundle\Migrations\Schema\v1_4\OroTrackerBundle;
+use Oro\Bundle\TrackingBundle\Migrations\Schema\v1_4\OroTrackerBundle as OroTrackingBundle_v1_4;
+use Oro\Bundle\TrackingBundle\Migrations\Schema\v2_0\OroTrackingBundle as OroTrackingBundle_v2_0;
 
 /**
  * @SuppressWarnings(PHPMD.TooManyMethods)
  * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  */
-class OroTrackingBundleInstaller implements Installation
+class OroTrackingBundleInstaller implements Installation, RenameExtensionAwareInterface
 {
+    /** @var RenameExtension */
+    private $renameExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRenameExtension(RenameExtension $renameExtension)
+    {
+        $this->renameExtension = $renameExtension;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getMigrationVersion()
     {
-        return 'v1_10';
+        return 'v2_0';
     }
 
     /**
@@ -43,11 +57,13 @@ class OroTrackingBundleInstaller implements Installation
         $this->addOroTrackingEventForeignKeys($schema);
         $this->addOroTrackingWebsiteForeignKeys($schema);
 
-        OroTrackerBundle::addOrganization($schema);
+        OroTrackingBundle_v1_4::addOrganization($schema);
 
         $this->addOroTrackingVisitEventForeignKeys($schema);
         $this->addOroTrackingEventDictionaryForeignKeys($schema);
         $this->addOroTrackingVisitForeignKeys($schema);
+
+        OroTrackingBundle_v2_0::updateTrackingVisitEvent($schema, $queries, $this->renameExtension);
     }
 
     /**
