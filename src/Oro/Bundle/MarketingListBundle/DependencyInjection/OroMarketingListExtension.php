9cf0d39c9b8577de58479fc3196885666f8d651f
Calin Bolea <calin.bolea@evozon.com>
2016-11-23 11:12:18 +0200

    CRM-6338: Move marketing-related bundles to a separate package - Created feature config - Added configuration class - Loaded configuration in bundle extension - Added marketing configuration tab and added feature config to it

--- src/Oro/Bundle/MarketingListBundle/DependencyInjection/OroMarketingListExtension.php
+++ src/Oro/Bundle/MarketingListBundle/DependencyInjection/OroMarketingListExtension.php
@@ -9,13 +9,28 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class OroMarketingListExtension extends Extension
 {
+    const ALIAS = 'oro_marketing';
+
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form.yml');
+
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAlias()
+    {
+        return self::ALIAS;
     }
 }
