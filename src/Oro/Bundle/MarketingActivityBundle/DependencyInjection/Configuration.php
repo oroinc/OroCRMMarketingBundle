d0fe9caee02436eec524c1908e10e06af2bc3547
Sergey Stupachenko <sstupachenko@magecore.com>
2016-12-30 14:33:11 +0000

    ISDOT-77: Marketing Activity Bundle Skeleton

---
+++ src/Oro/Bundle/MarketingActivityBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oro\Bundle\MarketingActivityBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_marketing_activity');
+
+        return $treeBuilder;
+    }
+}
