2aabaaf95b948b2398a570a5c634602a6815af13
Ievgenii Myshusta <imyshusta@magecore.com>
2017-02-17 12:37:19 +0200

    ISDOT-144: Setup Marketing Activities feature

--- src/Oro/Bundle/MarketingActivityBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/MarketingActivityBundle/DependencyInjection/Configuration.php
@@ -5,6 +5,8 @@ namespace Oro\Bundle\MarketingActivityBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
+
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -14,6 +16,13 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('oro_marketing_activity');
+
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                'feature_enabled' => ['value' => true],
+            ]
+        );
 
         return $treeBuilder;
     }
