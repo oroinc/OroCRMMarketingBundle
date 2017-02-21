fbe7d910fac9113ea1d3f2ec895a18418b34d3a1
Bogdan Tsyganovskiy <btsyganovskiy@magecore.com>
2017-02-22 01:10:21 +0200

    ISDOT-94: Collect send and bounce statistics  - updated collect activities action  - updated unit tests  - added ability to disable process logger to avoid memory leaks on processes execution

---
+++ src/Oro/Bundle/MarketingActivityBundle/Model/ActivityFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Oro\Bundle\MarketingActivityBundle\Model;
+
+use Oro\Bundle\CampaignBundle\Entity\Campaign;
+use Oro\Bundle\CampaignBundle\Entity\EmailCampaign;
+use Oro\Bundle\EntityExtendBundle\Entity\AbstractEnumValue;
+use Oro\Bundle\EntityExtendBundle\Provider\EnumValueProvider;
+use Oro\Bundle\MarketingActivityBundle\Entity\MarketingActivity;
+
+class ActivityFactory
+{
+    /**
+     * @var EnumValueProvider
+     */
+    protected $enumProvider;
+
+    /**
+     * @param EnumValueProvider $enumProvider
+     */
+    public function __construct(EnumValueProvider $enumProvider)
+    {
+        $this->enumProvider = $enumProvider;
+    }
+
+    /**
+     * @param Campaign $campaign
+     * @param string $entityClass
+     * @param int $entityId
+     * @param \DateTime $actionDate
+     * @param string $type
+     * @param int $relatedCampaignId
+     * @param string $relatedCampaignClass
+     *
+     * @return MarketingActivity
+     */
+    public function create(
+        Campaign $campaign,
+        $entityClass,
+        $entityId,
+        \DateTime $actionDate,
+        $type,
+        $relatedCampaignId,
+        $relatedCampaignClass = EmailCampaign::class
+    ) {
+        $marketingActivity = new MarketingActivity();
+        $marketingActivity->setCampaign($campaign);
+        $marketingActivity->setEntityClass($entityClass);
+        $marketingActivity->setEntityId($entityId);
+        $marketingActivity->setType($this->getActivityType($type));
+        $marketingActivity->setRelatedCampaignId($relatedCampaignId);
+        $marketingActivity->setRelatedCampaignClass($relatedCampaignClass);
+        $marketingActivity->setActionDate($actionDate);
+
+        return $marketingActivity;
+    }
+
+    /**
+     * @param $id
+     * @return AbstractEnumValue
+     */
+    protected function getActivityType($id)
+    {
+        return $this->enumProvider->getEnumValueByCode(MarketingActivity::TYPE_ENUM_CODE, $id);
+    }
+}
