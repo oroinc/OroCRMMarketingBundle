6463a4b63c6b3cbb94c7471ba941f96a143b9274
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2017-08-29 11:17:39 +0300

    CRM-8430: Fix tracking.php for new sensio distribution bundle (#12746)
    * CRM-8430: Fix tracking.php for new sensio distribution bundle
    - fixed tracking.php
    - added behat test

--- src/Oro/Bundle/TrackingBundle/Resources/public/lib/tracking.php
+++ src/Oro/Bundle/TrackingBundle/Resources/public/lib/tracking.php
@@ -99,6 +99,7 @@ function passDataToApplication($url)
 
     fillTrackingData($_GET);
 
+    require_once __DIR__ . '/../app/autoload.php';
     require_once __DIR__ . '/../app/bootstrap.php.cache';
     require_once __DIR__ . '/../app/AppKernel.php';
     $kernel = new AppKernel('prod', false);
