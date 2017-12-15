d7229efcd1c60c585b37bbc65a6acbbf6d511a60
Vova Soroka <vsoroka@oroinc.com>
2017-12-15 20:36:36 +0200

    BAP-15849: Upgrade 2.x LTS releases to latest Symfony 2.8.x version (… (#15562)

--- src/Oro/Bundle/MarketingListBundle/Entity/MarketingList.php
+++ src/Oro/Bundle/MarketingListBundle/Entity/MarketingList.php
@@ -630,7 +630,7 @@ class MarketingList extends ExtendMarketingList
      */
     public function __toString()
     {
-        return $this->getName();
+        return (string)$this->getName();
     }
 
     /**
