514ac430b789c8cf5b4ead9ad8fd051116c54937
Vova Soroka <vsoroka@oroinc.com>
2018-05-21 11:43:26 +0300

    Remove getName() and getBlockPrefix() methods from API form types (#18287)
    - als move AbstractPatchableApiType to SoapBundle as it is used only in old API, that is not a part of ApiBundle

--- src/Oro/Bundle/MarketingListBundle/Form/Type/MarketingListRemovedItemApiType.php
+++ src/Oro/Bundle/MarketingListBundle/Form/Type/MarketingListRemovedItemApiType.php
@@ -2,7 +2,7 @@
 
 namespace Oro\Bundle\MarketingListBundle\Form\Type;
 
-use Oro\Bundle\ApiBundle\Form\Type\AbstractPatchableApiType;
+use Oro\Bundle\SoapBundle\Form\Type\AbstractPatchableApiType;
 
 class MarketingListRemovedItemApiType extends AbstractPatchableApiType
 {
