817af3c46586070d0facab4f4a5ec8448a73d4cd
Yurii Muratov <ymuratov@oroinc.com>
2018-10-18 06:52:56 +0300

    BAP-17090: Provide compatibility with PHP 7.2 (#18847)
    - remove ext-mcrypt from platform dependencies
    - rename mcrypt chiper service
    - add openssl to system requirements documentation
    - fix Symfony Requirements
    - fix get_class() function usages
    - fix each() function usage
    - fix count() function usage
    - fix cssmin library
    - reconfigure CI builds

--- src/Oro/Bundle/CampaignBundle/Tests/Unit/Form/EventListener/TransportSettingsEmailTemplateListenerTest.php
+++ src/Oro/Bundle/CampaignBundle/Tests/Unit/Form/EventListener/TransportSettingsEmailTemplateListenerTest.php
@@ -6,6 +6,7 @@ use Oro\Bundle\CampaignBundle\Entity\EmailCampaign;
 use Oro\Bundle\CampaignBundle\Form\EventListener\TransportSettingsEmailTemplateListener;
 use Oro\Bundle\MarketingListBundle\Entity\MarketingList;
 use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
@@ -55,6 +56,9 @@ class TransportSettingsEmailTemplateListenerTest extends \PHPUnit\Framework\Test
             ->expects($this->any())
             ->method('getType')
             ->will($this->returnValue($type));
+        $type->expects($this->any())
+            ->method('getInnerType')
+            ->willReturn(new SubmitType());
 
         $this->form
             ->expects($this->any())
