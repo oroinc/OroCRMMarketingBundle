040ad3ad839c7eda06aa6ee46ee48d71c2d82646
Andrey Yatsenco <ayatsenko@oroinc.com>
2021-11-30 19:34:36 +0200

    BAP-3938: Introduce config tree for all entity configs
    - Introduced EntityConfigInterface, FieldConfigInterface and EntityConfigInterface interfaces to validate entity config options
    - Added validation and processing of entity configs on ConfigManager::persist() call
    - Added documentation on how to extend the entity config configuration
    - Added configuration and documentation for all the available configuration options
    - Removed unused fields configs: `is_total` and `is_total_currency`
    - Removed unused entity configs: `totals_mapping` and  `note`
    - Created command `oro:entity:config:validation:dump-reference` to dump the entity config configuration refference
    - Config handler injection into entity config update queries used in migrations
    - Unused configs and scopes cleanup on the extend entity cache warmup and as platform's pre-update migration
    - Skipping allowed_types(enity_config.yml) validation if config value is present in incomming once(not default)
    - Fixed data audit metadata rendering after entity config cleanup
    - Fixed schema update issue after entity config cleanup
    - Fixed form types after config types restrictions
    
    Co-authored-by: Ivan Makarenkov <imakarenkov@oroinc.com>
    Co-authored-by: Serhii Miasnoi <smiasnoi@oroinc.com>

---
+++ src/Oro/Bundle/MarketingListBundle/EntityConfig/EntityFieldConfiguration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Oro\Bundle\MarketingListBundle\EntityConfig;
+
+use Oro\Bundle\EntityConfigBundle\EntityConfig\FieldConfigInterface;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+/**
+ * Provides validations field config for entity scope.
+ */
+class EntityFieldConfiguration implements FieldConfigInterface
+{
+    public function getSectionName(): string
+    {
+        return 'entity';
+    }
+
+    public function configure(NodeBuilder $nodeBuilder): void
+    {
+        $nodeBuilder
+            ->scalarNode('contact_information')
+                ->info('`string` enables you to change contact information (phone or email) for the entity. Each ' .
+                'contact_information type requires its own template. E.g. phone => ' .
+                '“OroMarketingListBundle:MarketingList/ExtendField:phone.html.twig”.')
+            ->end()
+        ;
+    }
+}
