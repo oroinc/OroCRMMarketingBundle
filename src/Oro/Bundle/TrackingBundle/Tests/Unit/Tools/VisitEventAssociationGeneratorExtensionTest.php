37e40f53edaf950f8730441d9e698ee8183bd134
Chetverikov Yevhen <103036822+chetverikovyevhen@users.noreply.github.com>
2022-05-16 11:41:52 +0300

    BAP-21346: Dependency error breaks installation of platform application (#32577)
    - add check exit code
    - added generation of default association methods
    - updated tests

--- src/Oro/Bundle/TrackingBundle/Tests/Unit/Tools/VisitEventAssociationGeneratorExtensionTest.php
+++ src/Oro/Bundle/TrackingBundle/Tests/Unit/Tools/VisitEventAssociationGeneratorExtensionTest.php
@@ -95,7 +95,7 @@ class VisitEventAssociationGeneratorExtensionTest extends \PHPUnit\Framework\Tes
             ],
             [
                 ['class' => TrackingVisitEvent::class],
-                false
+                true
             ],
             [
                 ['class' => 'Test\Entity', 'relation' => 'test'],
@@ -104,67 +104,82 @@ class VisitEventAssociationGeneratorExtensionTest extends \PHPUnit\Framework\Tes
         ];
     }
 
-    public function testGenerate()
+    /**
+     * @dataProvider getGenerateDataProvider
+     */
+    public function testGenerate(array $schema, string $expectedResultFileName): void
     {
-        $schema = [
-            'relationData' => [
-                [
-                    'field_id' => new FieldConfigId(
-                        'extend',
-                        'Test\Entity',
-                        ExtendHelper::buildAssociationName(
-                            'Test\TargetEntity1',
-                            VisitEventAssociationExtension::ASSOCIATION_KIND
-                        ),
-                        'manyToOne'
-                    ),
-                    'target_entity' => 'Test\TargetEntity1',
-                    'state' => 'Active'
-                ],
-                [
-                    'field_id' => new FieldConfigId(
-                        'extend',
-                        'Test\Entity',
-                        ExtendHelper::buildAssociationName(
-                            'Test\TargetEntity2',
-                            VisitEventAssociationExtension::ASSOCIATION_KIND
-                        ),
-                        'manyToOne'
-                    ),
-                    'target_entity' => 'Test\TargetEntity2',
-                    'state' => 'Active'
-                ],
-                [   // should be ignored because field type is not multipleManyToOne
-                    'field_id' => new FieldConfigId(
-                        'extend',
-                        'Test\Entity',
-                        ExtendHelper::buildAssociationName(
-                            'Test\TargetEntity3',
-                            VisitEventAssociationExtension::ASSOCIATION_KIND
-                        ),
-                        'manyToMany'
-                    ),
-                    'target_entity' => 'Test\TargetEntity3',
-                    'state' => 'Active'
-                ],
-                [   // should be ignored because field name is not match association naming conventions
-                    'field_id' => new FieldConfigId(
-                        'extend',
-                        'Test\Entity',
-                        'testField',
-                        'manyToOne'
-                    ),
-                    'target_entity' => 'Test\TargetEntity4',
-                    'state' => 'Active'
-                ]
-            ]
-        ];
-
         $class = new ClassGenerator('Test\Entity');
 
         $this->extension->generate($schema, $class);
 
-        $expectedBody = \file_get_contents(__DIR__ . '/Fixtures/generationAssociationResult.txt');
+        $expectedBody = \file_get_contents(__DIR__ . $expectedResultFileName);
         self::assertEquals(\trim($expectedBody), \trim($class->print()));
+    }
+
+    public function getGenerateDataProvider(): array
+    {
+        return [
+            'associations' => [
+                'schema' => [
+                    'relationData' => [
+                        [
+                            'field_id' => new FieldConfigId(
+                                'extend',
+                                'Test\Entity',
+                                ExtendHelper::buildAssociationName(
+                                    'Test\TargetEntity1',
+                                    VisitEventAssociationExtension::ASSOCIATION_KIND
+                                ),
+                                'manyToOne'
+                            ),
+                            'target_entity' => 'Test\TargetEntity1',
+                            'state' => 'Active'
+                        ],
+                        [
+                            'field_id' => new FieldConfigId(
+                                'extend',
+                                'Test\Entity',
+                                ExtendHelper::buildAssociationName(
+                                    'Test\TargetEntity2',
+                                    VisitEventAssociationExtension::ASSOCIATION_KIND
+                                ),
+                                'manyToOne'
+                            ),
+                            'target_entity' => 'Test\TargetEntity2',
+                            'state' => 'Active'
+                        ],
+                        [   // should be ignored because field type is not multipleManyToOne
+                            'field_id' => new FieldConfigId(
+                                'extend',
+                                'Test\Entity',
+                                ExtendHelper::buildAssociationName(
+                                    'Test\TargetEntity3',
+                                    VisitEventAssociationExtension::ASSOCIATION_KIND
+                                ),
+                                'manyToMany'
+                            ),
+                            'target_entity' => 'Test\TargetEntity3',
+                            'state' => 'Active'
+                        ],
+                        [   // should be ignored because field name is not match association naming conventions
+                            'field_id' => new FieldConfigId(
+                                'extend',
+                                'Test\Entity',
+                                'testField',
+                                'manyToOne'
+                            ),
+                            'target_entity' => 'Test\TargetEntity4',
+                            'state' => 'Active'
+                        ]
+                    ]
+                ],
+                'expectedResultFileName' => '/Fixtures/generationAssociationResult.txt',
+            ],
+            'only default association methods' => [
+                'schema' => [],
+                'expectedResultFileName' => '/Fixtures/generationDefaultAssociationMethodsResult.txt',
+            ],
+        ];
     }
 }
