6d4b59c46698a46211de715fe6f32295e56f10be
Vova Soroka <vsoroka@oroinc.com>
2025-03-13 19:29:14 +0200

    BAP-22964: Replace Symfony\Bridge\Doctrine\ManagerRegistry with Doctrine\Persistence\ManagerRegistry (#40731)

--- src/Oro/Bundle/CampaignBundle/Tests/Unit/Command/SendEmailCampaignsCommandTest.php
+++ src/Oro/Bundle/CampaignBundle/Tests/Unit/Command/SendEmailCampaignsCommandTest.php
@@ -2,53 +2,43 @@
 
 namespace Oro\Bundle\CampaignBundle\Tests\Unit\Command;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\CampaignBundle\Command\SendEmailCampaignsCommand;
 use Oro\Bundle\CampaignBundle\Entity\EmailCampaign;
 use Oro\Bundle\CampaignBundle\Entity\Repository\EmailCampaignRepository;
 use Oro\Bundle\CampaignBundle\Model\EmailCampaignSender;
 use Oro\Bundle\CampaignBundle\Model\EmailCampaignSenderBuilder;
 use PHPUnit\Framework\MockObject\MockObject;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class SendEmailCampaignsCommandTest extends \PHPUnit\Framework\TestCase
+class SendEmailCampaignsCommandTest extends TestCase
 {
-    private ManagerRegistry|MockObject $registry;
-
+    private ManagerRegistry|MockObject $doctrine;
     private EmailCampaignSenderBuilder|MockObject $emailCampaignSenderBuilder;
-
     private SendEmailCampaignsCommand $command;
-
-    private EmailCampaignRepository|MockObject $emailCampaignRepository;
-
-    private EntityManager|MockObject $entityManager;
-
-    private EmailCampaignSender|MockObject $emailCampaignSender;
 
     #[\Override]
     protected function setUp(): void
     {
-        $this->registry = $this->createMock(ManagerRegistry::class);
+        $this->doctrine = $this->createMock(ManagerRegistry::class);
         $this->emailCampaignSenderBuilder = $this->createMock(EmailCampaignSenderBuilder::class);
-        $this->emailCampaignRepository = $this->createMock(EmailCampaignRepository::class);
-        $this->entityManager = $this->createMock(EntityManager::class);
-        $this->emailCampaignSender = $this->createMock(EmailCampaignSender::class);
 
         $this->command = new SendEmailCampaignsCommand(
-            $this->registry,
+            $this->doctrine,
             $this->emailCampaignSenderBuilder
         );
     }
 
-    public function testShouldReturnNoEmailCampaignsFound()
+    public function testShouldReturnNoEmailCampaignsFound(): void
     {
-        $this->registry->expects(self::once())
+        $emailCampaignRepository = $this->createMock(EmailCampaignRepository::class);
+        $this->doctrine->expects(self::once())
             ->method('getRepository')
             ->with(EmailCampaign::class)
-            ->willReturn($this->emailCampaignRepository);
-
-        $this->emailCampaignRepository->expects(self::once())
+            ->willReturn($emailCampaignRepository);
+        $emailCampaignRepository->expects(self::once())
             ->method('findEmailCampaignsToSend')
             ->willReturn([]);
 
@@ -58,43 +48,44 @@ class SendEmailCampaignsCommandTest extends \PHPUnit\Framework\TestCase
         self::assertStringContainsString('No email campaigns to send', $tester->getDisplay());
     }
 
-    public function testShouldSendEmailForEmailCampaigns()
+    public function testShouldSendEmailForEmailCampaigns(): void
     {
         $firstEmailCampaign = (new EmailCampaign())->setName('First Campaign');
         $secondEmailCampaign = (new EmailCampaign())->setName('Second Campaign');
 
-        $this->registry->expects(self::once())
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->doctrine->expects(self::once())
             ->method('getManagerForClass')
             ->with(EmailCampaign::class)
-            ->willReturn($this->entityManager);
+            ->willReturn($entityManager);
 
-        $this->registry->expects(self::once())
+        $emailCampaignRepository = $this->createMock(EmailCampaignRepository::class);
+        $this->doctrine->expects(self::once())
             ->method('getRepository')
             ->with(EmailCampaign::class)
-            ->willReturn($this->emailCampaignRepository);
-
-        $this->emailCampaignRepository->expects(self::once())
+            ->willReturn($emailCampaignRepository);
+        $emailCampaignRepository->expects(self::once())
             ->method('findEmailCampaignsToSend')
             ->willReturn([
                 $firstEmailCampaign,
                 $secondEmailCampaign,
             ]);
 
+        $emailCampaignSender = $this->createMock(EmailCampaignSender::class);
         $this->emailCampaignSenderBuilder->expects(self::exactly(2))
             ->method('getSender')
-            ->willReturn($this->emailCampaignSender);
-
-        $this->emailCampaignSender->expects(self::exactly(2))
+            ->willReturn($emailCampaignSender);
+        $emailCampaignSender->expects(self::exactly(2))
             ->method('send');
 
-        $this->entityManager->expects(self::exactly(2))
+        $entityManager->expects(self::exactly(2))
             ->method('persist')
             ->withConsecutive(
                 [$firstEmailCampaign],
                 [$secondEmailCampaign],
             );
 
-        $this->entityManager->expects(self::once())
+        $entityManager->expects(self::once())
             ->method('flush');
 
         $tester = new CommandTester($this->command);
@@ -105,8 +96,8 @@ class SendEmailCampaignsCommandTest extends \PHPUnit\Framework\TestCase
         self::assertStringContainsString('Sending email campaign: Second Campaign', $tester->getDisplay());
         self::assertStringContainsString('Finished email campaigns sending', $tester->getDisplay());
         self::assertTrue($firstEmailCampaign->isSent());
-        self::assertEquals(\DateTime::class, get_class($firstEmailCampaign->getSentAt()));
+        self::assertInstanceOf(\DateTime::class, $firstEmailCampaign->getSentAt());
         self::assertTrue($secondEmailCampaign->isSent());
-        self::assertEquals(\DateTime::class, get_class($secondEmailCampaign->getSentAt()));
+        self::assertInstanceOf(\DateTime::class, $secondEmailCampaign->getSentAt());
     }
 }
